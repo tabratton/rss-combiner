7882e5ed547497132619c13e1d510c1785751dbf
Tyler Bratton <tylerbratton96@gmail.com>
2025-08-10 20:54:02 -0400

    fetch feeds in parallel, add caching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(duration_constructors_lite)]
+
 use axum::Router;
 use axum::extract::{FromRef, State};
 use axum::http::{StatusCode, header};
@@ -7,6 +9,10 @@ use bb8::Pool;
 use bb8_postgres::PostgresConnectionManager;
 use rss::{Channel, ChannelBuilder, Item};
 use std::str::FromStr;
+use std::sync::Arc;
+use std::time::Duration;
+use moka::future::{Cache, CacheBuilder};
+use tokio::task::JoinSet;
 use tokio_postgres::{Config, NoTls, Row};
 use tower::ServiceBuilder;
 use tower_http::cors::CorsLayer;
@@ -43,8 +49,13 @@ async fn main() -> Result<(), anyhow::Error> {
     let manager = PostgresConnectionManager::new(postgres_config, NoTls);
     let pool = Pool::builder().max_size(10).build(manager).await?;
 
+    let rss_cache = CacheBuilder::new(100)
+        .time_to_live(Duration::from_mins(25))
+        .build();
+
     let app_state = AppState {
-        db_pool: pool.clone(),
+        db_pool: pool,
+        rss_cache: rss_cache,
     };
 
     let layer = ServiceBuilder::new().layer(CorsLayer::permissive());
@@ -71,6 +82,7 @@ async fn main() -> Result<(), anyhow::Error> {
 #[derive(Clone)]
 struct AppState {
     db_pool: Pool<PostgresConnectionManager<NoTls>>,
+    rss_cache: Cache<String, Arc<Channel>>,
 }
 
 impl FromRef<AppState> for Pool<PostgresConnectionManager<NoTls>> {
@@ -79,12 +91,15 @@ impl FromRef<AppState> for Pool<PostgresConnectionManager<NoTls>> {
     }
 }
 
-#[instrument(skip(db_pool))]
+#[instrument(skip(app_state))]
 async fn get_rss_feed(
-    State(db_pool): State<Pool<PostgresConnectionManager<NoTls>>>,
+    State(app_state): State<AppState>,
 ) -> Result<impl IntoResponse, AppError> {
+    let db_pool = app_state.db_pool.clone();
+    let rss_cache = app_state.rss_cache.clone();
+
     let feeds = get_db_feeds(db_pool).await?;
-    let feed_items = get_feeds_by_url(feeds).await?;
+    let feed_items = get_feeds_by_url(feeds, rss_cache).await?;
 
     Ok((
         [(header::CONTENT_TYPE, "text/xml")],
@@ -110,17 +125,38 @@ async fn get_db_feeds(
 }
 
 #[instrument(skip(feeds))]
-async fn get_feeds_by_url(feeds: Vec<Feed>) -> Result<Vec<Item>, AppError> {
+async fn get_feeds_by_url(feeds: Vec<Feed>, rss_cache: Cache<String, Arc<Channel>>) -> Result<Vec<Item>, AppError> {
     let mut feed_items = Vec::new();
+    let mut join_set = JoinSet::new();
     for feed in feeds {
-        let content = reqwest::get(feed.url).await?.bytes().await?;
-        let channel: Channel = Channel::read_from(&content[..])?;
-        for item in channel.items {
-            feed_items.push(item);
-        }
+        let cache = rss_cache.clone();
+        join_set.spawn(async move {
+            get_feed_by_url(feed.url, cache).await
+        });
+    }
+
+    while let Some(Ok(res)) = join_set.join_next().await {
+        let channel = res?;
+        let items: Vec<Item> = channel.items.iter().map(|item| item.clone()).collect();
+        feed_items.extend(items);
     }
 
     Ok(feed_items)
+}
+
+#[instrument]
+async fn get_feed_by_url(url: String, rss_cache: Cache<String, Arc<Channel>>) -> Result<Arc<Channel>, AppError> {
+    let channel = match rss_cache.get(&url).await {
+        Some(channel) => channel,
+        None => {
+            let content = reqwest::get(&url).await?.bytes().await?;
+            let channel: Arc<Channel> = Arc::new(Channel::read_from(&content[..])?);
+            rss_cache.insert(url, channel.clone()).await;
+            channel
+        },
+    };
+
+    Ok(channel)
 }
 
 #[instrument(skip(feed_items))]
